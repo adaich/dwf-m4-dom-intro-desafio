8fcffe72e1f971b434fa60bdc72c3b15b9b4848c
adaich <arieldaich@gmail.com>
2021-09-21 03:02:10 -0300

    primer commit

--- index.js
+++ index.js
@@ -25,6 +25,21 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listEl = document.querySelector(".lista");
+  const listItems = document.querySelectorAll(".lista li");
+  listItems.forEach((i) => {
+    listEl.removeChild(i);
+  });
+
+  cosasQueAprendimos.forEach((i) => {
+    const liEl = document.createElement("li");
+    liEl.textContent = i.tema;
+    if (i.class) {
+      liEl.classList.add(i.class);
+    }
+    listEl.appendChild(liEl);
+  });
+}
 
 main();
